031241d73ddf94e515b22837d5e7bac080d5cd71
Jeremy Schiff <jschiff90@gmail.com>
2013-09-21 10:44:06 -0400

    Added goal trigger behavior

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalTrigger : MonoBehaviour {
+	public Collider playerCollider;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+    void OnTriggerEnter(Collider other) {
+		if (other == playerCollider) {
+			Application.Quit();
+		}
+    }
+}
