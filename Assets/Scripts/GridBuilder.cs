a48f356f85678d88ae1ac1e4824761559974fe03
Jeremy Schiff <jeremyschiff@Jeremys-MacBook-Air.local>
2013-09-27 08:53:26 -0400

    Added gridbuilder

---
+++ Assets/Scripts/GridBuilder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridBuilder : MonoBehaviour {
+	// Objects to replicate.
+	public GameObject[] objects;
+	// Determines direction and spacing for the x axis
+	public Vector3 xAxis;
+	// Determines direction and spacing for the y axis
+	public Vector3 yAxis;
+	// The object at 0, 0 will be here.
+	public Vector3 growFrom;
+	// Map with object ids to determine which objects go in which location.  
+	// Values in this map are used as indices to the objects array above.
+	public long[,] map;
+	
+	// Use this for initialization
+	void Start () {
+		int width = map.GetLength(0);
+		int height = map.GetLength(1);
+		Vector3[,] worldPositions = new Vector3[map.GetLength(0), map.GetLength(1)];
+		
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				worldPositions[i, j] = growFrom + (i * xAxis) + (j * yAxis);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MapReader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/**
+ * Reads map files.
+ */
+public class MapReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
