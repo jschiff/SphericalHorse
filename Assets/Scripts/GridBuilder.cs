b23a0bf6c73b15860c038b955190d4eb6f96fd70
Jeremy Schiff <jeremyschiff@Jeremys-MacBook-Air.local>
2013-09-27 21:06:59 -0400

    Grid spawning is working

--- Assets/Scripts/GridBuilder.cs
+++ Assets/Scripts/GridBuilder.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
+using System.IO;
 using System.Collections;
 
 public class GridBuilder : MonoBehaviour {
 	// Objects to replicate.
-	public GameObject[] objects;
+	public GameObject[] prefabs;
 	// Determines direction and spacing for the x axis
-	public Vector3 xAxis;
+	public Vector3 xAxis = new Vector3(0, 0, -1);
 	// Determines direction and spacing for the y axis
-	public Vector3 yAxis;
+	public Vector3 yAxis = Vector3.up;
 	// The object at 0, 0 will be here.
 	public Vector3 growFrom;
 	// Map with object ids to determine which objects go in which location.  
@@ -16,13 +17,46 @@ public class GridBuilder : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		map = new long[,]{
+			{-1, -1, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, -1}
+		};
+		
+		if (map == null || prefabs == null) return;
 		int width = map.GetLength(0);
 		int height = map.GetLength(1);
+		
+		Vector3[,] worldPositions = generatePositions(width, height, xAxis, yAxis);
+		InstantiatePrefabs(worldPositions, map, prefabs);
+	}
+	
+	private Vector3[,] generatePositions(int width, int height, Vector3 xAxis, Vector3 yAxis) {
 		Vector3[,] worldPositions = new Vector3[map.GetLength(0), map.GetLength(1)];
 		
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
 				worldPositions[i, j] = growFrom + (i * xAxis) + (j * yAxis);
+			}
+		}
+		
+		return worldPositions;
+	}
+	
+	private void InstantiatePrefabs(Vector3[,] worldPositions, long[,] map, GameObject[] objectDictionary) {
+		int width = map.GetLength(0);
+		int height = map.GetLength(1);
+		
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				long mapValue = map[i, j];
+				if (mapValue < 0) continue;
+				print(mapValue);
+				print(objectDictionary);
+				GameObject prefab = objectDictionary[mapValue];
+				if (prefab != null) {
+					GameObject instance = (GameObject)Instantiate(prefab);
+					instance.transform.position = worldPositions[i, j];
+				}
 			}
 		}
 	}
