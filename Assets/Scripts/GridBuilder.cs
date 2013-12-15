4617b5f82ee7ab9f896c89747c1f4ab1333184e2
Jeremy Schiff <jeremy@getperka.com>
2013-12-15 15:11:38 -0500

    work on png importer

--- Assets/Scripts/GridBuilder.cs
+++ Assets/Scripts/GridBuilder.cs
@@ -12,17 +12,19 @@ public class GridBuilder : MonoBehaviour {
 	// The object at 0, 0 will be here.
 	public Vector3 growFrom;
 	// Map with object ids to determine which objects go in which location.  
-	// Values in this map are used as indices to the objects array above.
+	// Values in this map are used as indices to the prefabs array above.
 	public long[,] map;
 	
 	// Use this for initialization
 	void Start () {
 		map = new long[,]{
 			{-1, -1, 0, 0, 0, 0, 0, 0},
-			{0, 0, 0, 0, 0, 0, 0, -1}
-		};
+			{0, 0, 0, 0, 0, 0, 0, -1}};
 		
-		if (map == null || prefabs == null) return;
+		if (map == null || prefabs == null) {
+			return;
+		}
+
 		int width = map.GetLength(0);
 		int height = map.GetLength(1);
 		
@@ -30,26 +32,38 @@ public class GridBuilder : MonoBehaviour {
 		InstantiatePrefabs(worldPositions, map, prefabs);
 	}
 	
-	private Vector3[,] generatePositions(int width, int height, Vector3 xAxis, Vector3 yAxis) {
+	private Vector3[,] generatePositions (int width, int height) {
 		Vector3[,] worldPositions = new Vector3[map.GetLength(0), map.GetLength(1)];
 		
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
-				worldPositions[i, j] = growFrom + (i * xAxis) + (j * yAxis);
+				worldPositions[i, j] = worldPosition(i, j);
 			}
 		}
 		
 		return worldPositions;
 	}
+
+	// Calculate the world position of a grid position on the fly
+	public static Vector3 worldPosition (int x, int y, Vector3 xAxis, Vector3 yAxis, Vector3 growFrom) {
+		return growFrom + (x * xAxis) + (y * yAxis);
+	}
+
+	// Calculate the world position of a grid position on the fly.
+	private Vector3 worldPosition (int x, int y) {
+		return worldPosition(x, y, this.xAxis, this.yAxis, this.growFrom);
+	}
 	
-	private void InstantiatePrefabs(Vector3[,] worldPositions, long[,] map, GameObject[] objectDictionary) {
+	private void InstantiatePrefabs (Vector3[,] worldPositions, long[,] map, GameObject[] objectDictionary) {
 		int width = map.GetLength(0);
 		int height = map.GetLength(1);
 		
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
 				long mapValue = map[i, j];
-				if (mapValue < 0) continue;
+				if (mapValue < 0) {
+					continue;
+				}
 				print(mapValue);
 				print(objectDictionary);
 				GameObject prefab = objectDictionary[mapValue];
--- Assets/Scripts/PNGImporter.cs
+++ Assets/Scripts/PNGImporter.cs
@@ -6,23 +6,23 @@ using System.Text;
 using System.Collections.Generic;
 
 namespace AssemblyCSharp {
-	public class PNGImporter {
-		public static string[,] buildMatrix(string mapPath, string dictionaryPath) {
-			var dictionary = readDictionary(dictionaryPath);
-			var bitmap = new Bitmap(mapPath);
-			var matrix = new string[bitmap.Width, bitmap.Height];
+public class PNGImporter {
+	public static string[,] buildMatrix (string mapPath, string dictionaryPath) {
+		var dictionary = readDictionary(dictionaryPath);
+		var bitmap = new Bitmap(mapPath);
+		var matrix = new string[bitmap.Width, bitmap.Height];
 			
-			for (int i = 0; i < bitmap.Width; i++) {
-				for (int j = 0; j < bitmap.Height; j++) {
-					var color = bitmap.GetPixel(i, j);
-					matrix[i, j] = dictionary[color];
-				}
+		for (int i = 0; i < bitmap.Width; i++) {
+			for (int j = 0; j < bitmap.Height; j++) {
+				var color = bitmap.GetPixel(i, j);
+				matrix[i, j] = dictionary[color];
 			}
-			
-			return matrix;
 		}
+			
+		return matrix;
+	}
 		
-		/**
+	/**
 		 * Dictionary file format is:
 		 * ASCII Encoded
 		 * One entry per line.
@@ -31,35 +31,56 @@ namespace AssemblyCSharp {
 		 * The first number is a 32 bit color with 8 bits each for Alpha, Red, Green, and Blue respectively.
 		 * The second number is an id to refer to a prefab.
 		 */
-		private static Dictionary<System.Drawing.Color, string> readDictionary(string dictionaryFilename){
-			var fStream = File.OpenRead(dictionaryFilename);
-			var reader = new StreamReader(fStream, Encoding.ASCII);
-			var dictionary = new Dictionary<System.Drawing.Color, string>();
-			
-			int lineNum = -1;
-			string line = null;
-			while(null != (line = reader.ReadLine())) {
-				lineNum++;
-				var tokens = line.Split(':');
-				if (tokens.Length == 2) {
-					var color = colorFromString(tokens[0]);
-					dictionary[color] = tokens[1];
-				}
-				else {
-					throw new IOException("Dictionary file format incorrect at line " + lineNum);
-				}
-			}
-			
-			return dictionary;
-		}
-		
-		private static System.Drawing.Color colorFromString(string color) {
-			int colorInt = int.Parse(color);
-			var alpha = (int)(colorInt | 0xFF000000) >> 6;
-			var red = (int)(colorInt | 0x00FF0000) >> 4;
-			var green = (int)(colorInt | 0x0000FF00) >> 2;
-			var blue = (int)colorInt | 0x000000FF;
-			return System.Drawing.Color.FromArgb(alpha, red, green, blue);
-		}
+	private static Dictionary<System.Drawing.Color, string> readDictionaryFile (string dictionaryFilename) {
+		var fStream = File.OpenRead(dictionaryFilename);
+		return readDictionaryStream(fStream);
 	}
+		
+	private static Dictionary<System.Drawing.Color, string> readDictionaryStream (Stream stream) {
+		var reader = new StreamReader(stream, Encoding.ASCII);
+		var dictionary = new Dictionary<System.Drawing.Color, string>();
+			
+		int lineNum = -1;
+		string line = null;
+		while (null != (line = reader.ReadLine())) {
+			lineNum++;
+				
+			line = line.Trim();
+			// Ignore comments
+			int commentStart = line.IndexOf('#');
+			if (commentStart > -1) {
+				line = line.Substring(0, commentStart);
+			}
+				
+			var tokens = line.Split(':');
+			if (tokens.Length == 2) {
+				var color = colorFromString(tokens[0]);
+				dictionary[color] = tokens[1];
+			}
+				else {
+				throw new IOException("Dictionary format incorrect at line " + lineNum);
+			}
+		}
+			
+		return dictionary;
+	}
+		
+	private static Dictionary<System.Drawing.Color, string> readDictionaryString (string dictionaryContents) {
+		MemoryStream stream = new MemoryStream();
+		StreamWriter writer = new StreamWriter(stream);
+		writer.Write(dictionaryContents);
+		writer.Flush();
+		stream.Position = 0;
+		return readDictionaryStream(stream);
+	}
+		
+	private static System.Drawing.Color colorFromString (string color) {
+		int colorInt = int.Parse(color);
+		var alpha = (int)(colorInt | 0xFF000000) >> 6;
+		var red = (int)(colorInt | 0x00FF0000) >> 4;
+		var green = (int)(colorInt | 0x0000FF00) >> 2;
+		var blue = (int)colorInt | 0x000000FF;
+		return System.Drawing.Color.FromArgb(alpha, red, green, blue);
+	}
+}
 }
