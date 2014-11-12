b9121b1e4a89a3d33b851581476c305081a56b3b
Jeremy Schiff <jschiff90@gmail.com>
2014-11-11 21:56:42 -0500

    Laying foundations for wall module system

--- Assets/Scripts/Controls/WASDQEControl.cs
+++ Assets/Scripts/Controls/WASDQEControl.cs
@@ -4,52 +4,73 @@ using System.Collections;
 /**
  * Three dimensional control via WASDQE keys
  */
-namespace AssemblyCSharp {
-public class WASDQEControl : MonoBehaviour {
-	public Rigidbody body;
-	public float forceMultiplier = 1f;
-	public bool ignorePhysics = false;
-	public bool xAxis = true;
-	public bool yAxis = true;
-	public bool zAxis = false;
+namespace AssemblyCSharp
+{
+	public class WASDQEControl : MonoBehaviour
+	{
+		public Rigidbody body;
+		public float forceMultiplier = 1f;
+		public bool ignorePhysics = false;
+		public bool xAxis = true;
+		public bool yAxis = true;
+		public bool zAxis = false;
+		private static IDictionary keyMappings = new Hashtable ();
 	
-	private static IDictionary keyMappings = new Hashtable();
-	
-	// Use this for initialization
-	void Start () {
-		if (xAxis) {
-			keyMappings[KeyCode.A] = Vector3.left;
-			keyMappings[KeyCode.D] = Vector3.right;
-		}
+		// Use this for initialization
+		void Start ()
+		{
+			if (xAxis) {
+				keyMappings ["Horizontal"] = Vector3.right;
+				//keyMappings[KeyCode.A] = Vector3.left;
+				//keyMappings[KeyCode.D] = Vector3.right;
+			}
 		
-		if (yAxis) {
-			keyMappings[KeyCode.W] = Vector3.up;
-			keyMappings[KeyCode.S] = Vector3.down;
-		}
+			if (yAxis) {
+				keyMappings ["Vertical"] = Vector3.up;
+				//keyMappings[KeyCode.W] = Vector3.up;
+				//keyMappings[KeyCode.S] = Vector3.down;
+			}
 		
-		if (zAxis) {
-			keyMappings[KeyCode.Q] = Vector3.forward;
-			keyMappings[KeyCode.E] = Vector3.back;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float timeElapsed = Time.deltaTime;
-		foreach (KeyCode key in keyMappings.Keys) {
-			if (Input.GetKey(key)) {
-				Vector3 vec = (Vector3)keyMappings[key] * forceMultiplier * timeElapsed;
-				
-				if (ignorePhysics) {
-					body.velocity = Vector3.zero;
-					body.angularVelocity = Vector3.zero;
-					body.MovePosition(body.position + vec);
-				}
-				else {
-					body.AddForce(vec);	
-				}
+			if (zAxis) {
+				keyMappings [KeyCode.Q] = Vector3.forward;
+				keyMappings [KeyCode.E] = Vector3.back;
 			}
 		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+			float timeElapsed = Time.deltaTime;
+			foreach (string axisName in keyMappings.Keys) {
+				float axisValue = Input.GetAxis (axisName);
+				if (axisValue != 0.0f) {
+					Vector3 vec = (Vector3)keyMappings [axisName] * axisValue;
+								
+					if (ignorePhysics) {
+						body.velocity = Vector3.zero;
+						body.angularVelocity = Vector3.zero;
+						body.MovePosition (body.position + vec);
+					} else {
+						body.AddForce (vec);	
+					}
+				}
+			}
+						
+			/*
+						foreach (KeyCode key in keyMappings.Keys) {
+								if (Input.GetKey (key)) {
+										Vector3 vec = (Vector3)keyMappings [key] * forceMultiplier * timeElapsed;
+				
+										if (ignorePhysics) {
+												body.velocity = Vector3.zero;
+												body.angularVelocity = Vector3.zero;
+												body.MovePosition (body.position + vec);
+										} else {
+												body.AddForce (vec);	
+										}
+								}
+						}
+						*/
+		}
 	}
-}
 }
---
+++ Assets/Scripts/Modules/Module.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// A module is a thing embedded into a wall that changes how projectiles interact with the wall.
+public class Module : MonoBehaviour
+{
+	public static GameObject host;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
