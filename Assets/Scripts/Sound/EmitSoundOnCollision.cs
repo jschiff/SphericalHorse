40c506a7339e92405a5ddaa276284c14f7175104
Jeremy Schiff <jeremy@getperka.com>
2015-10-04 18:52:42 -0400

    Normalize some sounds

--- Assets/Scripts/Sound/EmitSoundOnCollision.cs
+++ Assets/Scripts/Sound/EmitSoundOnCollision.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class EmitSoundOnCollision : MonoBehaviour {
-	public float enterVelocityThreshold = 2;
+	public float volumeMax = 6f;
+	public float volumeMin = 1f;
 	public AudioClip[] audioClips;
 
 	void OnCollisionEnter2D (Collision2D collision) {
@@ -10,18 +12,24 @@ public class EmitSoundOnCollision : MonoBehaviour {
 		Vector3 relativeVelocity = collision.relativeVelocity;
 		Vector3 projected = Vector3.Project(relativeVelocity, collisionNormal);
 		Vector2 projected2 = new Vector2(projected.x, projected.y);
-		if (projected2.magnitude > enterVelocityThreshold) {
-			GetComponent<AudioSource>().PlayOneShot(randomAudioClip());
+		float impact = projected2.magnitude;
+		if (impact > volumeMin) {
+			impact -= volumeMin;
+			float maxImpact = volumeMax - volumeMin;
+			float volumeScale = Mathf.Max(1.0f, impact / maxImpact);
+			GetComponent<AudioSource>().PlayOneShot(randomAudioClip(), volumeScale);
 		}
 	}
 
 	void OnCollisionEnter (Collision collision) {
+		/*
 		Vector3 collisionNormal = collision.contacts[0].normal;
 		Vector3 relativeVelocity = collision.relativeVelocity;
 		Vector3 projected = Vector3.Project(relativeVelocity, collisionNormal);
 		if (projected.magnitude > enterVelocityThreshold) {
 			GetComponent<AudioSource>().PlayOneShot(randomAudioClip());
 		}
+		*/
 	}
 
 	private AudioClip randomAudioClip () {
